b4dcb98fda58b67a53698747489973306252b7ee
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-12-24 18:38:58 +0530

    save all

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import navbar from './components/navbar/navbar'
+
+function App() {
+  return (
+    <div className='app'>
+      <navbar/>
+    </div>
+  )
+}
+
+export default App
