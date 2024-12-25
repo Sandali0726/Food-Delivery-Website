ebcd84d7ba299d7972da47e5d4051ffd6330f764
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-12-25 13:52:54 +0530

    feat: implement Cart, Home, and PlaceOrder pages with routing and styles

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -20,11 +20,12 @@ const Navbar = () => {
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
         <img src={assets.basket_icon} alt="" />
-        <div className="dot">
+        <div className="dot"></div>
+        </div>
         <button>sign in</button>
         </div>
-        </div>
-        </div>
+        
+        
         
         
     </div>
---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import './Cart.css'
+import React from 'react'
+
+function Cart() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
