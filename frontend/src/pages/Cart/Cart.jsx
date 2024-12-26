ffc38879803616b8b8808068e13d117598ce74e4
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-12-26 13:50:58 +0530

    feat: add cart total calculation and display; enhance Cart component styling and structure

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -9,7 +9,7 @@ function Cart() {
   return (
     <div className='cart'>
       <div className="cart-items">
-        <div className="class-items-title">
+        <div className="cart-items-title">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
@@ -20,8 +20,66 @@ function Cart() {
         </div>
         <br />
         <hr />
+        {food_list.map((item,index) => {
+          if(cartItems[item._id]>0){
+            return (
+              <div>
+                <div className='cart-items-title cart-item-item'>
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${item.price*cartItems[item._id]}</p>
+                <p onClick = { () => removeFromCart(item._id)} className='cross'>x</p>
+              </div>
+              <hr />
+              </div>
+
+
+
+              
+            )
+          }
+
+        })}
       </div>
-      
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+          <div className="cart-total-details">
+          <p>Subtotal</p>
+          <p>{0}</p>
+          </div>
+          <hr />
+
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>{2}</p>
+          </div>
+          <hr />
+
+          <div className="cart-total-details">
+            <b>Total</b>
+            <b>{0}</b>
+          </div>
+          
+        </div>
+        <button>PROCEED TO CHECKOUT</button>
+      </div>
+      <div className="cart-promocode">
+        <div>
+          <p>
+            If you have a promo code, Enter it here
+          </p>
+          <div className='cart-promocode-input'>
+            <input type="text" placeholder='promo code' />
+            <button>Submit</button>
+
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
