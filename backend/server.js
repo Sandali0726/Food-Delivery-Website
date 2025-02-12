739157d4f846c3b4723eb0244b8712bb092c9cc7
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-02-12 12:32:42 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/db.js';
-import morgan from "morgan";
+
 
 import foodRouter from "./routes/foodRoutes.js";
 
