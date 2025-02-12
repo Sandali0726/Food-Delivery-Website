7032ac0e9c04dfb82b01111ac78a528ac73eb6e8
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2025-02-12 12:38:50 +0530

    save

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,7 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () => {
-    mongoose.connection.on('connected', () => {
-        console.log("DB Connected");
-    });
+export const connectDB = async() => {
+    await mongoose.connect
+    ("mongodb+srv://jayawardhanasandali2:Sandali6254560@cluster0.tzyx1.mongodb.net/food-del").then(()=> console.log("DB connected"))
+}   
 
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/food-del`);
-    } catch (error) {
-        console.error("DB Connection Error:", error);
-    }
-};
-
-export default connectDB;
